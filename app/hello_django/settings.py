67dd30691658f170922ed0f64feb926ea03afdd5
Zelalem Mekonnen <zelliott33@gmail.com>
2021-12-11 11:55:05 -0600

    aws-setting.py correction

--- app/hello_django/settings.py
+++ app/hello_django/settings.py
@@ -27,7 +27,8 @@ SECRET_KEY = 'django-insecure-rd_q8=#5%)xlhl8!b8o410apqror1(3^2j6m3jr#xr&!+!8%84
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['ec2-3-208-39-9.compute-1.amazonaws.com','0.0.0.0', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['ec2-3-208-39-9.compute-1.amazonaws.com',
+                 '0.0.0.0', 'localhost', '127.0.0.1']
 #ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
 
 # Application definition
